0134472030980316890cc10e071bd677be13fc1f
KeniaKodigo <kenia.paz@kodigo.org>
2025-09-10 20:41:30 -0600

    conexion a base de datos

--- views/test.php
+++ views/test.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "../models/TaskModel.php";
+require_once "../config/database.php";
 
 //print_r(TaskModel::all());
 
@@ -10,12 +11,16 @@ require_once "../models/TaskModel.php";
 
 //TaskModel::edit(4, "Estilos al boton","agregar nuevos estilos al boton de la seccion de inventario");
 
-if(isset($_POST['taskById'])){
-    $id = $_POST['taskById']; //5
-echo $id;
-}
+// if(isset($_POST['taskById'])){
+//     $id = $_POST['taskById']; //5
+// echo $id;
+// }
 
 
 //$task = TaskModel::getById($id);
 
 //formulario con los datos de la tarea (editar)
+
+
+$pdo = Connection::getInstance()->getConnection();
+print_r($pdo);
